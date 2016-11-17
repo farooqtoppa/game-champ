08283fd184994fa6d224588f61ce4353b668c9f6
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 18:29:20 -0500

    required passport localstrategy port and app

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ var bodyParser      = require('body-parser');
 var mongoose        = require('mongoose');
 var logger          = require('morgan');
 var methodOverride  = require('method-override');
+var passport        = require('passport');
+var LocalStrategy   = require('passport-local').Strategy;
+var port            = process.env.PORT || 4000;
+var app             = express();
