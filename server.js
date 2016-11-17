18d46c08b8b331def34d249b91999ec377104ea1
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 18:42:12 -0500

    middleware for public/dirname

--- server.js
+++ server.js
@@ -27,3 +27,5 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: false} ));
 app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+
