b5c8fd8c4580ed44c887651113df1868ad4fb489
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 18:23:28 -0500

    required express, bodyParser, mongoose, logger, methodov

---
+++ server.js
@@ -0,0 +1,8 @@
+// =====================================
+// MODULES
+// =====================================
+var express         = require('express');
+var bodyParser      = require('body-parser');
+var mongoose        = require('mongoose');
+var logger          = require('morgan');
+var methodOverride  = require('method-override');
