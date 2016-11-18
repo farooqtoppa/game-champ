b974073c3c51aff86a23d4e22c604000a80b707c
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 22:01:58 -0500

    added product schema in product.js

---
+++ models/product.js
@@ -0,0 +1,11 @@
+var mongoose  = require('mongoose');
+
+var productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  category: String,
+  imageurl: String
+});
+
+module.exports = mongoose.model('product', productSchema);
