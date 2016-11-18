8cb99d0d1f2409025dc617da9148b40361eba801
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 21:57:48 -0500

    required product schema in orderItem.js and created orderItemSchema

--- models/order.js
+++ models/order.js
@@ -2,3 +2,7 @@ var mongoose          = require('mongoose');
 
 var OrderItemSchema   = require('./orderitem').schema;
 var UserSchema        = require('./user').schema;
+
+OrderItemSchema.virtual('subtotal').get(function(){
+  return this.quantity * this.product.price;
+})
