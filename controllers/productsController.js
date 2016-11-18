d6534b72b99bd9534a0897044b7507c3ff6bacc4
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 21:41:43 -0500

    authorize function created in products controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,14 @@ router          = express.Router();
 
 var Product     = require('../models/product');
 
-
+var authorize = function(req, res, next) {
+  if(!req.user) {
+    res.json(401, 'unauthorized')
+  }
+  else{
+    next()
+  }
+}
 
 
 
