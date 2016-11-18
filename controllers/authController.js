64c3c6d5a0542641fcc7e296bebbb0808cbc114d
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 19:55:01 -0500

    sign up rout for back end in authController.js added

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,23 @@ var LocalStrategy   = require('passport-local').Strategy;
 
 var User = require('../models/user.js');
 
+// ===================================
+// SIGN UP
+// ===================================
+router.post('/signup', function(req, res) {
+  // Register new user
+  User.register(new User(
+    { username: req.body.username }),
+    req.body.password, function(err, user) {
+      if (err) {
+        return res.json({ user: user });
+      }
+      else{
+        res.json({status: 201, message: 'success', user: user});
+      }
+    });
+});
+
 
 
 
