f81e062107278ebc9b7055b3d95c6208c92e960c
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 22:08:34 -0500

    OrderSchema added in order.js

--- models/order.js
+++ models/order.js
@@ -6,3 +6,29 @@ var UserSchema        = require('./user').schema;
 OrderItemSchema.virtual('subtotal').get(function(){
   return this.quantity * this.product.price;
 })
+
+var OrderSchema = new mongoose.Schema({
+  items: [OrderItemSchema],
+  user: UserSchema,
+  createdAt: Date,
+  updatedAt: Date
+},{
+  toObject: {
+    virtuals: true
+  },
+  toJSON: {
+    virtuals: true
+  }
+});
+OrderSchema.virtual('total').get(function(){
+  return this.items.map(function(item) {
+    return item.subtotal;
+  }).reduce(function(m, e){ return m + e; },0);
+});
+OrderSchema.pre('save', function(next){
+  if (!this.createdAt) { this.createdAt = new Date(); }
+  this.updatedAt = new Date();
+  next();
+})
+
+module.exports = mongoose.model('Order', OrderSchema);
