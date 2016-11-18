1a05318bf32789f29ea4333357e541dfe4b08b60
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 23:10:51 -0500

    added angular module for app and main router

---
+++ public/app.js
@@ -0,0 +1,10 @@
+(function() {
+  angular.module('GameChamp', ['ui.router'])
+  .config(MainRouter);
+
+  function MainRouter($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  }
+
+})() // ends IIFEE
+console.log('app.js');
