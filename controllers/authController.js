38cf71d657a3847dd6c498c8367e5f3bf626855c
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-17 19:47:25 -0500

    required express, router, passport, and user model in Auth contr.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,6 @@
+var express         = require('express');
+var router          = express.Router();
+var passport        = require('passport');
+var LocalStrategy   = require('passport-local').Strategy;
 
+var User = require('../models/user.js');
