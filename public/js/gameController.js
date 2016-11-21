e4a944b1e6fdcc07590ae6294a92e3383adcdb12
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-21 17:59:14 -0500

    . added to save user to db on login and empty cart on log out. Also getProducts function updated to search by string

--- public/js/gameController.js
+++ public/js/gameController.js
@@ -5,25 +5,68 @@
   GameController.$inject = ['$http', '$state', '$cart', '$scope'];
 
   function GameController($http, $state, $cart, $scope) {
+
+    this.all = [];
+
     // refers back to controller
     var self = this;
+
+    //save user data on log in
+    $scope.$on('UserLoggedIn', function(eventObj, data){
+      self.user = data;
+    });
+
+    //make sure to empty cart when user logs out
+    $scope.$on('UserLoggedOut', function(eventObj){
+      self.cart = [];
+      self.user = null;
+    });
+
+    // boolean for empty cart
+    this.cartHasItems = false;
+    this.cartTotal = 0;
+    this.existingOrders = false;
+
+    // Initialize search value to '' to return all products in database
+    this.searchStr = '';
+
+    // used for reseting quantity in dropdown menu of product page
+    this.quantityAtShopIndex = {};
+
+    // used for reseting quantity in dropdown menu of cart page
+    this.quantityAtCartIndex = {};
+    this.cart = [];
 
     //=================================
     // ALL PRODUCTS FROM DB
     // ================================
-    this.getProducts = function() {
-      $http.get('/api/products')
+    this.getProducts = function(searchStr,categoryOrName) {
+      $http.get('/api/products/')
       .catch(function(error){
         console.log(error);
       })
       .then(function(response){
-        console.log('These are all of the products ',response.data.products);
+        if (categoryOrName === 'name') {
+          self.all = response.data.products.filter(
+            function(el)  {
+            return el.name.toLowerCase().indexOf(self.searchStr.toLowerCase()) !== -1;
+            // return this product if the name contains the search string
+          });
+        }
+        if (categoryOrName === 'category') {
+          self.all = response.data.products.filter(
+            function(el)  {
+            return el.category.toLowerCase().indexOf(searchStr.toLowerCase()) !== -1;
+            // return this product if the name contains the search string
+          });
+        }
       })
       .catch(function(error){
         console.log(error);
       });
     }
-  }
+
+  } // ends GameController
 
 })()
 
