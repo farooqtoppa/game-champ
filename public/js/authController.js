8fea6f14eea0d5fc1d4129a8030a3cf57abba629
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-20 21:52:47 -0500

    creating auth controller in authController.js that will be used seperaely for authentication

---
+++ public/js/authController.js
@@ -0,0 +1,17 @@
+(function(){
+  angular.module('GameChamp')
+  .controller('AuthController', AuthController);
+
+  AuthController.$inject = ['$http', '$state'];
+
+  function AuthController($http, $state) {
+
+  } // ends AuthController
+
+
+
+
+
+})(); // ends IIFEE
+
+console.log('authController.js')
